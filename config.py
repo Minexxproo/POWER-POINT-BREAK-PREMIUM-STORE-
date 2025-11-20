faedf40af73091d77bb1eafdf4dbc39fe6e0be54
Miss Soma <162959468+Minexxproo@users.noreply.github.com>
2025-11-20 21:01:15 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# --- গুরুত্বপূর্ণ সেটিংস ---
+# ১. আপনার বট টোকেন (BotFather থেকে পাওয়া)
+BOT_TOKEN = "8293368030:AAGmoa5b4MimIFbLJjttGz60lGYmEHK4seA" 
+
+# ২. আপনার টেলিগ্রাম ইউজার আইডি (Admin's Telegram User ID)
+# এটি অবশ্যই সংখ্যায় হতে হবে। যেমন: 1234567890
+ADMIN_ID = 5692210187 
+ADMIN_USERNAME = "@MinexxProo"
+
+# ৩. পেমেন্টের জন্য মোবাইল ব্যাংকিং নম্বর
+PAYMENT_NUMBER = "01877576843"
